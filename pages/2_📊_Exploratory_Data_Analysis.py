ec45d654852ead3705da435bfd6c56da1b7a68d7
sorawitr0607 <sorawit.r@tgh.co.th>
2024-10-11 14:39:39 +0700

    new

--- pages/2_📊_Exploratory_Data_Analysis.py
+++ pages/2_📊_Exploratory_Data_Analysis.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_pandas_profiling import st_profile_report
+from streamlit_ydata_profiling import st_profile_report
 import pandas as pd
 import time
 # from pandas_profiling import ProfileReport
